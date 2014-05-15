2b00d3e258a4be3c62a25c503a5e16f8af17d8ec
Aron Jones <aron.jones@gmail.com>
2014-05-15 11:23:31 -0400

    add python package file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-resize',
+    description='A simple resize filter for Django',
+    url='http://github.com/defrex/django-resize/',
+    license='MIT',
+    author='Aron Jones',
+    author_email='aron.jones@gmail.com',
+    packages=find_packages(),
+    version='0.1.0',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django>=1.5',
+    ],
+)
